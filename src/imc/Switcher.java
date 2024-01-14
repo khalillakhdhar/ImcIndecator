fde9f35efbe403715a1a265584546953a1482b8d
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2024-01-14 09:47:09 +0100

    app

---
+++ src/imc/Switcher.java
@@ -0,0 +1,38 @@
+package imc;
+
+public class Switcher {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=11;
+		switch(x)
+		{
+		case 1:
+			System.out.println("lundi");
+			break;
+		case 2:
+			System.out.println("mardi");
+			break;
+		case 3:
+			System.out.println("mercredi");
+			break;
+		case 4:
+			System.out.println("jeudi");
+			break;
+		case 5:
+			System.out.println("vendredi");
+			break;
+		case 6:
+			System.out.println("samedi");
+			break;
+		case 7:
+			System.out.println("dimanche");
+			break;
+		default: 
+			System.out.println("numéro incorrect!");
+		}
+		
+
+	}
+
+}
